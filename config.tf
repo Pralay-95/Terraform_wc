8116a337d0b791bf694123f53f0b707d089c641c
Pralay Ninave <129081571+Pralay-95@users.noreply.github.com>
2023-12-10 18:33:50 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.6.2"
+    }
+  }
+
+#scenario 2 using s3 backend 
+backend "s3" {
+bucket = "pralay"
+key = " terra-key.pem"
+region = "us-west-2"
+
+  }
+}
 provider "aws" {
     region = "${var.AWS_REGION}"
 }
+
